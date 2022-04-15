fdef4eb9993a4c18d35ca33a165d0890ac882684
TrnChThnh1301 <thanhtran010301@gmail.com>
2022-04-15 14:52:09 +0700

    Lab3 Homework

--- Lab3/AimsProject/src/Package1/Aims.java
+++ Lab3/AimsProject/src/Package1/Aims.java
@@ -4,6 +4,9 @@ public class Aims {
 
 	public static void main(String[] args) {
 		Cart anOrder = new Cart();
+		
+		anOrder.qtyOrdered = 0;
+		
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
 		anOrder.addDigitalVideoDisc(dvd1);
 		
@@ -13,8 +16,8 @@ public class Aims {
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 		anOrder.addDigitalVideoDisc(dvd3);
 		
-		System.out.println("Total Cost is ");
-		System.out.println(anOrder.totalCost());
+		System.out.println("Total cost is " + anOrder.totalCost());
+		anOrder.removeDigitalVideoDisc(dvd3);
 	}
 
 }
--- Lab3/AimsProject/src/Package1/Cart.java
+++ Lab3/AimsProject/src/Package1/Cart.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Cart {
 	public static final int MAX_NUMBERS_ORDERED = 20;
-	public DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
 	public int qtyOrdered = itemsOrdered.length;
 	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
 		if(qtyOrdered >= 20) {
@@ -13,30 +13,30 @@ public class Cart {
 		else {
 			itemsOrdered[qtyOrdered] = disc;
 			qtyOrdered++;
-			System.out.println("The disc has been added to cart!\n");
+			System.out.println("The disc " + disc.getTitle() + " has been added to cart!\n");
 		}
+		//System.out.println("There are " + qtyOrdered + " disc in the cart.\n");
 	}
-	public void removeDigitalVideoDisc(DigitalVideoDisc disc, int index) {
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
 		if(qtyOrdered < 1) {
-			System.out.println("There is no disc o remove!\n");
+			System.out.println("There is no disc to remove!\n");
 		}
 		else {
-			Scanner input = new Scanner(System.in);
-			System.out.println("Nhap vi tri muon xoa: ");
-			index = input.nextInt();
 			DigitalVideoDisc[] newArr = new DigitalVideoDisc[itemsOrdered.length - 1];
+			qtyOrdered -= 1;
+			System.out.println("The disc " + disc.getTitle() + " has been removed from the cart!\n");
+			System.out.println("There are " + qtyOrdered + " disc left on the cart:");
 			for(int i = 0,j = 0; i < qtyOrdered; i++) {
-				if(i != index) {
+				if(itemsOrdered[i] != disc) {
 				newArr[j++] = itemsOrdered[i];
+				System.out.println(itemsOrdered[i].getTitle());
 				}
 			}
-			input.close();
 		}
-		
 	}
 	public float totalCost() {
-		float sum = 0;
-		for(int i = 0 ; i < itemsOrdered.length; i++) {
+		float sum = 0f;
+		for(int i = 0 ; i < qtyOrdered ; i++) {
 			sum = sum + itemsOrdered[i].getCost();
 		}
 		return sum;
